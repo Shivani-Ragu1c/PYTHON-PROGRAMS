306527090497041128c668f091b1a307cb5829c8
Shivani-Ragu1c <133478482+Shivani-Ragu1c@users.noreply.github.com>
2023-06-03 21:04:50 +0530

    Update READ A FILE LINE BY LINE.py
     write a python program to read a file line by line and print it

--- READ A FILE LINE BY LINE.py
+++ READ A FILE LINE BY LINE.py
@@ -1,14 +1,15 @@
+# write a python program to read a file line by line and print it.
+
+
+# INPUT:
 
 # by using readlines()
-
 L= ["python\n", "lab\n", "work\n"]
 
-# write a file
 f1 = open('myfile.txt', 'w')
 f1.writelines(L)
 f1.close()
 
-# open a file
 f1 = open('myfile.txt', 'r')
 lines = f1.readlines()
 
@@ -19,18 +20,13 @@ for line in lines:
 
 
 
-
-
 # by using readline()
-
 L= ["python\n", "lab\n", "work\n"]
 
-# Write a file
 f1 = open('myfile.txt', 'w')
 f1.writelines((L))
 f1.close()
 
-# open a file
 f1 = open('myfile.txt', 'r')
 lines = f1.readline()
 count = 0
@@ -40,17 +36,13 @@ for line in lines:
 
 
 
-
 # by using read()
-
 L= ["python\n", "lab\n", "work\n"]
 
-# Write a file
 f1 = open('myfile.txt', 'w')
 f1.writelines((L))
 f1.close()
 
-# open a file
 f1 = open('myfile.txt', 'r')
 lines = f1.read()
 count = 0
@@ -61,20 +53,58 @@ for line in lines:
 
 
 # using for loop
-
 L= ["python\n", "lab\n", "work\n"]
 
-# Write a file
 f1 = open('myfile.txt', 'w')
 f1.writelines(L)
 f1.close()
 
-# opening file
 f1 = open('myfile.txt', 'r')
 count = 0
 for line in f1:
     count += 1
     print(count,line)
 
-# close a file
 f1.close()
+
+
+# OUTPUT:
+
+'''1 python
+
+2 lab
+
+3 work
+
+1 p
+2 y
+3 t
+4 h
+5 o
+6 n
+7
+
+1 p
+2 y
+3 t
+4 h
+5 o
+6 n
+7
+
+8 l
+9 a
+10 b
+11
+
+12 w
+13 o
+14 r
+15 k
+16
+
+1 python
+
+2 lab
+
+3 work'''
